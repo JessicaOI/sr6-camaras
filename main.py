77ae30d6eb79f13cabcef73ec8f5fd0e854c5716
Jessica Ortiz <70552512+JessicaOI@users.noreply.github.com>
2022-10-08 22:55:45 -0600

    listo phtoshoot

--- main.py
+++ main.py
@@ -12,10 +12,40 @@ render = Render(1000, 1000)
 texture = Texture('textureF.bmp')
 
 
+#descmentarear solo el blque de codigo que se quiera renderizar
+
 #medium
 #               eye, center, up
-render.lookAt(V3(-0.2,0,20), V3(0,0,0), V3(0,1,0))
-render.glObjModel('formica.obj',translate=(0,0,0), scale=(0.3,0.3,0.3), rotate=(0,0.5,0.36), texture=texture)
+# render.lookAt(V3(-0.2,0,20), V3(0,0,0), V3(0,1,0))
+# render.glObjModel('formica.obj',translate=(0,0,0), scale=(0.3,0.3,0.3), rotate=(0,-1.5,0), texture=texture)
+
+# #imagen final
+# render.glFinish('Medium.bmp')
+
+
+#Low
+#               eye, center, up
+# render.lookAt(V3(0.1,-1,20), V3(0,0,0), V3(0,1,0))
+# render.glObjModel('formica.obj',translate=(0,0,0), scale=(0.3,0.3,0.3), rotate=(-0.5,-1.5,0), texture=texture)
+
+# #imagen final
+# render.glFinish('Low.bmp')
+
+
+
+#High
+#               eye, center, up
+# render.lookAt(V3(0.1,1,20), V3(0,0,0), V3(0,1,0))
+# render.glObjModel('formica.obj',translate=(0,0,0), scale=(0.3,0.3,0.3), rotate=(0.5,-1.5,0), texture=texture)
+
+# #imagen final
+# render.glFinish('High.bmp')
+
+
+#Dutch
+#               eye, center, up
+render.lookAt(V3(-0.5,0,20), V3(0,0,0), V3(0,1,0))
+render.glObjModel('formica.obj',translate=(0,0,0), scale=(0.3,0.3,0.3), rotate=(0.5,-2,0), texture=texture)
 
 #imagen final
-render.glFinish('output.bmp')
+render.glFinish('Dutch.bmp')
